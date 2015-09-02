71c7e38599c8f48abdb5bbb1198d6bdf2bb4059a
Nick V <wrdrvr@gmail.com>
2015-09-02 19:31:41 -0400

    Fixed downloading

--- build2.py
+++ build2.py
@@ -5,10 +5,9 @@ import os
 import sys
 from io import TextIOWrapper
 import DataMap
-from pprint import pprint
+from ftplib import FTP
 import urllib.request as urllib2
 import urllib.parse as urlparse
-
 '''
 This script will parse data from
 http://www.fec.gov/finance/disclosure/ftpdet.shtml
@@ -171,41 +170,17 @@ def get_file(type,YR):
     download_file(url)
 
 def download_file(url, desc=None):
-    print(url)
-    u = urllib2.urlopen(url)
+    print("Downloading " + url)
     scheme, netloc, path, query, fragment = urlparse.urlsplit(url)
     filename = os.path.basename(path)
+    fl = urllib2.urlopen(url)
     if not filename:
         filename = 'downloaded.file'
     if desc:
         filename = os.path.join(desc, filename)
-
     with open(DATA_DIR+'/'+filename, 'wb') as f:
-        meta = u.info()
-        meta_func = meta.getheaders if hasattr(meta, 'getheaders') else meta.get_all
-        meta_length = meta_func("Content-Length")
-        file_size = None
-        if meta_length:
-            file_size = int(meta_length[0])
-        print("Downloading: {0} Bytes: {1}".format(url, file_size))
-
-        file_size_dl = 0
-        block_sz = 8192
-        while True:
-            buffer = u.read(block_sz)
-            if not buffer:
-                break
-
-            file_size_dl += len(buffer)
-            f.write(buffer)
-
-            status = "{0:16}".format(file_size_dl)
-            if file_size:
-                status += "   [{0:6.2f}%]".format(file_size_dl * 100 / file_size)
-            status += chr(13)
-            print(status, end="")
+        f.write(fl.read())
         print()
-
     return filename
 
 
